2528f4ca614dbdeff80d453a7f28e4926deab28c
YogendrasinhChavda <ychavda011@gmail.com>
2018-05-25 15:36:20 +0530

    [ADD/IMP] Migrated mrp_bom_by_percentage module.

---
+++ mrp_bom_by_percentage/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+{
+    "name": "MRP BoM By Percentage",
+    "version": "11.0.1.0.0",
+    "author": "OdooMRP team,"
+              "AvanzOSC,"
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+    "license": "AGPL-3",
+    "category": "Manufacturing",
+    "website": "http://www.odoomrp.com",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/mrp_bom_view.xml",
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
